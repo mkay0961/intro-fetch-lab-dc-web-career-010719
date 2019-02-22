7579fc6d6e7974161d77894c2fdcabc1d7d6b281
mkay0961 <mkay0961@gmail.com>
2019-02-22 13:05:12 -0500

    Done.

--- numbers.js
+++ numbers.js
@@ -1,1 +1,88 @@
 // Write your numbers code in this file!
+const baseURL = "http://numbersapi.com/"
+let currYear = new Date().getFullYear()
+let allnumberDiv = document.getElementById('all-the-numbers')
+let allnumbtn = document.getElementById('all-numbers-button')
+
+let yearDiv = document.getElementById('year-history')
+let factbtn = document.getElementById('number-one')
+let factdiv = document.getElementById('one-facts')
+let randommathfact = document.getElementById('random-math-fact')
+let enterNumber = document.getElementById('pick-a-number')
+ document.addEventListener('DOMContentLoaded', onDOM)
+
+allnumbtn.addEventListener('click', getallfacts)
+
+
+function getallfacts() {
+  allnumberDiv.innerText = ""
+  console.log("workign");
+  for (var i = 0; i < 100; i++) {
+    fetch(`${baseURL}random/trivia`)
+      .then(res => res.text())
+      .then(text => printallfacts(text));
+    console.log(`got ${i}`);
+  }
+}
+
+function printallfacts(data) {
+  console.log("ypoo");
+  let div = document.createElement('div')
+  div.innerText = `-${data}\n`
+  allnumberDiv.appendChild(div)
+}
+
+function onDOM(){
+  setInterval(getYearFact,5000)
+
+}
+
+function getYearFact(){
+  fetch(`${baseURL}${currYear}/year`)
+    .then(res => res.text())
+    .then(text => printYearfact(text));
+
+}
+
+function printYearfact(data){
+  console.log(data);
+  yearDiv.innerText += `${data}\n\n`
+  currYear -= 1
+}
+
+
+console.log("working");
+
+
+
+
+
+
+factbtn.addEventListener('click', getfacts)
+enterNumber.addEventListener('change', factsOnChange)
+
+
+function factsOnChange() {
+  console.log('heeeeeeyyyyy')
+  // debugger
+  if (!isNaN(parseInt(enterNumber.value))) {
+    fetch(`${baseURL}${enterNumber.value}/math`)
+      .then(res => res.text())
+      .then(text => printMathFact(text));
+  } else {randommathfact.innerText = 'Get Lost!'}
+  }
+
+function printMathFact(text) {
+  randommathfact.innerText = text
+
+}
+
+
+function getfacts() {
+  fetch(`${baseURL}1`)
+    .then(res => res.text())
+    .then(text => printFact(text));
+}
+function printFact(data){
+  factdiv.innerText = data
+}
--- swapi.js
+++ swapi.js
@@ -1,1 +1,43 @@
 // Write your swapi code in this file!
+let crawlbtn = document.getElementById('crawlBtn')
+let crawldiv = document.getElementById('crawlDiv')
+let planetBtn = document.getElementById('findPlanet')
+let planetInput = document.getElementById('planetInput')
+let planetDiv = document.getElementById('planetData')
+
+
+crawlBtn.addEventListener('click', getOpeningCrawl)
+planetBtn.addEventListener('click', getPlanet)
+
+function getPlanet(e) {
+  e.preventDefault()
+  let planetInputValue = planetInput.value
+  if((planetInputValue >= 1) && (planetInputValue <= 60)){
+    fetch(`https://swapi.co/api/planets/${planetInputValue}/`)
+      .then(res => res.json())
+      .then(json => planet(json));
+  }else{
+    planetDiv.innerText = "Wrong input. Enter a number between 1 - 60"
+  }
+  // debugger
+
+}
+
+
+function getOpeningCrawl() {
+  fetch('https://swapi.co/api/films/1/')
+    .then(res => res.json())
+    .then(json => crawl(json));
+}
+
+
+function planet(data){
+  console.log(data)
+  planetDiv.innerText = `Name: ${data["name"]} \nClimate: ${data["climate"]}`
+}
+
+function crawl(data){
+  console.log(data);
+  crawldiv.innerText = data["opening_crawl"]
+
+}
